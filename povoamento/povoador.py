6adf7d57373d8ab4b5321335ce861a561f73b9b2
AntonioG70 <48025387+AntonioG70@users.noreply.github.com>
2021-06-08 19:19:09 +0100

    creating animals props triples

--- povoamento/povoador.py
+++ povoamento/povoador.py
@@ -1,23 +1,38 @@
 from jjcli import *
 import requests as reqs
 
-types = ["Mammals", "Reptiles", "Fish", "Birds", "Amphibians", "Invertebrates", "Insects"]
+types = ["mammals", "reptiles", "fish", "birds", "amphibians", "invertebrates", "insects"]
 
-def get_pag_tipo(id):
-    idLow = id.lower()
+def get_animal_tipo(tipo):
     headers = {'User-Agent': 'PostmanRuntime/7.26.8'}
-    resp = reqs.get('https://a-z-animals.com/animals/' + idLow, headers=headers) 
+    resp = reqs.get('https://a-z-animals.com/animals/' + tipo, headers=headers) 
     resp.raise_for_status()
-    return resp.text
+    c1 = findall(r'">Types of.*List of', resp.text)
+    c2 = findall(r'">([A-Za-z ]*)<\/a>', c1[0])
+    return c2
 
-c = get_pag_tipo("mammals")   
+#c = get_animal_tipo("fish")   
+#print(c)
 
-c1 = findall(r'">Types of.*List of', c)
-c2 = findall(r'">([A-Za-z ]*)<\/a>', c1[0])
-print(c2)
+def criaClassificacao(animal):
+    headers = {'User-Agent': 'PostmanRuntime/7.26.8'}
+    resp = reqs.get('https://a-z-animals.com/animals/' + animal, headers=headers) 
+    resp.raise_for_status()
+    c1 = findall(r'Aardvark Scientific Classification.*Aardvark Conservation Status<\/h2', resp.text)
+    c2 = findall(r'([\sA-Za-z]*)<\/dt.*?([\sA-Za-z]*)<\/dd', c1[0])
+    for key, value in c2:
+        if key != "Scientific Name":
+            s = f""":{animal.capitalize()} :has{key.replace(" ", "")} :{value.replace(" ", "_")};"""
+            print(s)
+        else:
+            s = f""":{animal.capitalize()} :{key.replace(" ", "").lower()} '{value.replace(" ", "_")}';"""
+            print(s)
+    return c2
 
+criaClassificacao("aardvark")
 """
 for t in types:
     a = get_pag_tipo(t)
     print(c)
 """
+
