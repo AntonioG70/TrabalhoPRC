5f167f886df0b8415dd4b82f425edddcb5a2e066
CoelhoLado <naac.1999@gmail.com>
2021-06-22 10:56:29 +0100

    Fixed graphbd utils / added animal list

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var graphdb = require('../utils/graphdb')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/animals', function(req, res, next) {
+    let query = `SELECT ?a WHERE {
+                    ?a rdf:type :Animal .
+                }`
+
+    graphdb.execQuery(query)
+        .then(dados => {
+            let pubs = dados.data.results.bindings
+            pubs = pubs.map(elem => {
+                return graphdb.pair2Value(elem.a)
+            })
+            res.send(pubs)
+        })
+        .catch(error => {
+            res.send(error)
+        })
 });
 
 module.exports = router;
--- API/utils/graphdb.js
+++ API/utils/graphdb.js
@@ -5,17 +5,17 @@ var prefixes = `
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX noInferences: <http://www.ontotext.com/explicit>
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX : <https://www.nuno-aac.github.io/ontologias/emd#>
+        PREFIX : <http://www.semanticweb.org/antóniocarvalho/ontologies/animals#>
         `
 
 exports.execQuery = async function (query) {
-    var getLink = "http://localhost:7200/repositories/emd?query="
+    var getLink = "http://localhost:7200/repositories/Animals?query="
     var encoded = encodeURIComponent(prefixes + query)
     return axios.get(getLink + encoded)
 }
 
 exports.execTransaction = async function (query) {
-    var postLink = "http://localhost:7200/repositories/emd/statements"
+    var postLink = "http://localhost:7200/repositories/Animals/statements"
     var encoded = encodeURIComponent(prefixes + query)
     var response
     try {
