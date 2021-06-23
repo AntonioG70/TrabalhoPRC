7a2e7e4783cb29ba1f182b011c4f6ecf7946c2e8
CoelhoLado <naac.1999@gmail.com>
2021-06-23 17:20:51 +0100

    frontpage done

---
+++ frontend/animals/src/components/HomeMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-menu offset-y top>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn flat v-bind="attrs" v-on="on" color="white">
+                        <v-icon left color="black">mdi-plus</v-icon>
+                        <span>{{ title }}</span>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item
+                        v-for="link in links"
+                        :key="link.text"
+                        link
+                        :to='link.route'
+                    >
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+</template>
+
+<script>
+
+    export default {
+        name: 'HomeMenu',
+        props: ['links','title'],
+    }
+
+</script>
--- frontend/animals/src/views/Home.vue
+++ frontend/animals/src/views/Home.vue
@@ -1,15 +1,88 @@
 <template>
-  <hello-world />
+    <div class="background">
+        <div class="logo-header">
+            <v-expand-x-transition>
+                <v-card class="logo" v-show="logoShow">
+                    <v-img
+                        :src="require('../assets/animals.png')"
+                        contain
+                        height="15vw"
+                    />
+                </v-card>
+            </v-expand-x-transition>
+        </div>
+        <div class="buttons">
+            <HomeMenu title='Animal' :links='animalTypes'/>
+            <HomeMenu title='Locations' :links='locations'/>
+            <HomeMenu title='Classification' :links='locations'/>
+            <v-btn flat v-bind="attrs" v-on="on" color="white">
+                <v-icon left color="black">mdi-magnify</v-icon>
+                 <span>Advanced Search</span>
+            </v-btn>
+        </div>
+    </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+    import HomeMenu from '../components/HomeMenu.vue'
+    export default {
+        name: 'Home',
+        data () {
+            return {
+                logoShow: false,
+                animalTypes: [{ text: 'Mammals', route: '/animals?type=Mammals' },
+                              { text: 'Fish', route: '/animals?type=Fish' },
+                              { text: 'Reptiles', route: '/animals?type=Reptiles' },
+                              { text: 'Insects', route: '/animals?type=Insects' },
+                              { text: 'Birds', route: '/animals?type=Birds' },],
+                locations: [{ text: 'Africa', route: '/animals?location=Africa' },
+                            { text: 'Europe', route: '/animals?location=Europe' },
+                            { text: 'America', route: '/animals?location=America' },
+                            { text: 'Asia', route: '/animals?location=Asia' },
+                            { text: 'Antarctica', route: '/animals?location=Antarctica' },
+                            { text: 'Oceania', route: '/animals?location=Oceania' },]
+            } 
+        },
+        mounted () {
+            setTimeout(() => this.logoShow = true, 500)
+        },
+        components: {
+            HomeMenu
+        }
+    }
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
 </script>
+
+<style scoped>
+    .background {
+        background: linear-gradient(to right, #4d8030 20%, #9dd756 100%);
+        width: 100vw;
+        height: 100vh;
+    }
+    .logo {
+        width: 40vw;
+        height: 20vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-shadow: inset 5px 5px 15px 3px rgba(0,0,0,0.65) !important;
+    }
+
+    .logo-header {
+        width: 100vw;
+        height: 70vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .buttons {
+        width: 60vw;
+        margin: 0vw 20vw;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        align-self: center;
+    }
+
+</style>
